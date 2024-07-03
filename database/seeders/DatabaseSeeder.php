535c665da4dabd31339523065faed1a0e6ef1fdc
MoeSouki <mohamadelsouki49@gmail.com>
2024-07-03 15:28:42 +0400

    changed the project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,5 +10,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
 
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
 }
