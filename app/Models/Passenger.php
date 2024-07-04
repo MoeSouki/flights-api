da3aa309f4f4ad774e01cae64d57037310712edf
MoeSouki <mohamadelsouki49@gmail.com>
2024-07-04 11:30:55 +0400

    added soft delete

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Passenger extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes; // soft deletes allows you to mark records as deleted without actually removing them from the database.
     protected $table = 'passengers';
 
     protected $guarded = [
@@ -18,4 +19,6 @@ class Passenger extends Model
         'date_of_birth',
         'passport_expiry_date',
     ];
+    // a type of security vulnerability that occurs when an application code allows user-provided
+    // data to be used to set properties on an object without verifying that the user has the right to do so.
 }
