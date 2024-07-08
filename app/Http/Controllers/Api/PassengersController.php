eff02bb2941ca13d79f9d563b94ee3e48b857577
MoeSouki <mohamadelsouki49@gmail.com>
2024-07-08 17:09:58 +0400

    Create an API that retrieves all the users with pagination, filtering, and sorting

--- app/Http/Controllers/Api/PassengersController.php
+++ app/Http/Controllers/Api/PassengersController.php
@@ -8,9 +8,37 @@ use App\Http\Controllers\Controller;
 
 class PassengersController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $passengers = Passenger::paginate(10);
+        $query = Passenger::query();
+        if ($request->has('first_name')) {
+            $query->where('first_name', 'like', '%' . $request->input('first_name') . '%');
+
+        }
+
+        if ($request->has('last_name')) {
+            $query->where('last_name', 'like', '%' . $request->input('last_name') . '%');
+        }
+
+        if ($request->has('email')) {
+            $query->where('email', 'like', '%' . $request->input('email') . '%');
+        }
+
+        if ($request->has('date_of_birth')) {
+            $query->whereDate('date_of_birth', $request->input('date_of_birth'));
+        }
+        if ($request->has('passport_expiry_date')) {
+            $query->whereDate('passport_expiry_date', $request->input('passport_expiry_date'));
+        }
+
+        if ($request->has('sort_by') && $request->has('sort_order')) {
+            $query->orderBy($request->input('sort_by'), $request->input('sort_order'));
+        } elseif ($request->has('sort_by')) {
+            $query->orderBy($request->input('sort_by'), 'asc');
+        }
+
+
+        $passengers = $query->paginate(10);
 
         return response()->json($passengers);
     }
