0a2637d04b46a00324646a5e61f3b57d781ff6f0
MoeSouki <mohamadelsouki49@gmail.com>
2024-07-09 11:50:15 +0400

    Create Flight API

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Flight;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class FlightsController extends Controller
 {
-    //
+    public function index()
+    {
+        $flights = QueryBuilder::for(Flight::class)
+            ->allowedFilters([AllowedFilter::exact('id'), 'number', 'departure_city', 'arrival_city', 'departure_time', 'arrival_time'])
+            ->allowedSorts(['id', 'number', 'departure_city', 'arrival_city', 'departure_time', 'arrival_time'])
+            ->paginate(100);
+
+
+        return response()->json($flights);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FlightController;
 use App\Http\Controllers\Api\PassengersController;
+use App\Http\Controllers\FlightsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/passengers', [PassengersController::class, 'index']);
+Route::get('/flights', [FlightsController::class, 'index']);
