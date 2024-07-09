de6a69a4069be52ff584ee9862b9e4833abb932e
MoeSouki <mohamadelsouki49@gmail.com>
2024-07-09 11:19:53 +0400

    Fix the API that retrieves passengers with pagination, filtering, and sorting

--- app/Http/Controllers/Api/PassengersController.php
+++ app/Http/Controllers/Api/PassengersController.php
@@ -5,41 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Models\Passenger;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class PassengersController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Passenger::query();
-        if ($request->has('first_name')) {
-            $query->where('first_name', 'like', '%' . $request->input('first_name') . '%');
-
-        }
-
-        if ($request->has('last_name')) {
-            $query->where('last_name', 'like', '%' . $request->input('last_name') . '%');
-        }
-
-        if ($request->has('email')) {
-            $query->where('email', 'like', '%' . $request->input('email') . '%');
-        }
-
-        if ($request->has('date_of_birth')) {
-            $query->whereDate('date_of_birth', $request->input('date_of_birth'));
-        }
-        if ($request->has('passport_expiry_date')) {
-            $query->whereDate('passport_expiry_date', $request->input('passport_expiry_date'));
-        }
-
-        if ($request->has('sort_by') && $request->has('sort_order')) {
-            $query->orderBy($request->input('sort_by'), $request->input('sort_order'));
-        } elseif ($request->has('sort_by')) {
-            $query->orderBy($request->input('sort_by'), 'asc');
-        }
-
-
-        $passengers = $query->paginate(10);
+        $passengers = QueryBuilder::for(Passenger::class)
+            ->allowedFilters(['first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry_date'])
+            ->allowedSorts(['first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry_date'])
+            ->paginate(10);
 
         return response()->json($passengers);
+
     }
 }
