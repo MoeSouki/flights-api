c5baedece97b1e219085ce5aa0aec45b5890e8d5
MoeSouki <mohamadelsouki49@gmail.com>
2024-07-09 11:43:48 +0400

    Add id to passenger API

--- app/Http/Controllers/Api/PassengersController.php
+++ app/Http/Controllers/Api/PassengersController.php
@@ -6,15 +6,17 @@ use App\Models\Passenger;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class PassengersController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         $passengers = QueryBuilder::for(Passenger::class)
-            ->allowedFilters(['first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry_date'])
-            ->allowedSorts(['first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry_date'])
-            ->paginate(10);
+            ->allowedFilters([AllowedFilter::exact('id'), 'first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry_date'])
+            ->allowedSorts(['id', 'first_name', 'last_name', 'email', 'date_of_birth', 'passport_expiry_date'])
+            ->paginate(100);
+
 
         return response()->json($passengers);
 
